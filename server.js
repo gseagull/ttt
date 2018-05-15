88eadf98eeca04248e771de3f78b6adecccc1f3a
guy shahaf <guy@algomizer.com>
2018-05-15 11:14:36 +0300

    a

--- server.js
+++ server.js
@@ -1,35 +1,67 @@
-const express = require('express');
-const path = require('path');
+const io = require('socket.io')();
+//const express = require('express')();
+const port = process.env.PORT || 3000;
+var socket=io.listen(port);
+console.log('listening on port ', port);
 
-const app = express();
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
-
-// Put all API endpoints under '/api'
-app.get('/api/passwords', (req, res) => {
-  const count = 5;
-
-  
-  passwords=Array(9).fill("sss");
-  // Return them as json
-  res.json(passwords);
-
-  console.log(`Sent ${count} passwords`);
-});
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/build/index.html'));
-});
-
-const port = process.env.PORT || 8080;
-app.listen(port);
-
-//const io2 = socketIO(server);
- app.on('connection', (socket) => {
+/*
+const server = express()
+  .listen(port, () => console.log(`Listening on ${ port }`));
+ 
+const io2 = socketIO(server);
+ io2.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
-console.log(`Password generator listening on ${port}`);
+*/
+io.on('connection', (client) => {
+  client.on('subscribeToTimer', (interval) => {
+    console.log('client is subscribing to timer with interval ', interval);
+    setInterval(() => {
+      client.emit('timer', new Date());
+    }, interval);
+  });
+  client.on('myData', (message) => {
+    console.log('client message ', message);
+	console.log('client.id ', client.id);
+	
+	//socket.broadcast.emit('myData',message); 
+    //io.emit('myData',message,include_self=false); 
+	//io.emit('myData',message); 
+  });
+  
+});
+
+io.on('connection', socket => {
+
+  socket.on('myData', boardId => {
+	console.log('aaa ', boardId);
+    socket.join(`board${boardId}`);
+  });
+
+  socket.on('myData', message => {
+	console.log('44444444444444 ', message);
+    //const room = `board${card.board}`;
+    //socket.broadcast.to(room).emit('card/create', card);
+	 
+	
+	setTimeout(() => {
+	  socket.broadcast.emit('myData',message);;
+	}, 50);
+  });
+});
+
+
+
+
+/*
+socket.sockets.on('connection', function (socket) {
+    console.log('A client is connected!');
+  socket.on('myData', message => {
+	console.log('bbb ', message);
+    //const room = `board${card.board}`;
+    //socket.broadcast.emit('myData',message);
+	//socket.broadcast.to(room).emit('card/create', card);
+  });	
+});
+*/
