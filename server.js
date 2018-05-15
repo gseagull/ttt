525590b4b114d0199e48289dfe6bea6b1bfefd76
guy shahaf <guy@algomizer.com>
2018-05-15 09:59:43 +0300

    s

--- server.js
+++ server.js
@@ -1,67 +1,34 @@
-const io = require('socket.io')();
-//const express = require('express')();
-const port = process.env.PORT || 3000;
-var socket=io.listen(port);
-console.log('listening on port ', port);
+const express = require('express');
+const path = require('path');
+const generatePassword = require('password-generator');
 
-/*
-const server = express()
-  .listen(port, () => console.log(`Listening on ${ port }`));
- 
-const io2 = socketIO(server);
- io2.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
-});
-*/
-io.on('connection', (client) => {
-  client.on('subscribeToTimer', (interval) => {
-    console.log('client is subscribing to timer with interval ', interval);
-    setInterval(() => {
-      client.emit('timer', new Date());
-    }, interval);
-  });
-  client.on('myData', (message) => {
-    console.log('client message ', message);
-	console.log('client.id ', client.id);
-	
-	//socket.broadcast.emit('myData',message); 
-    //io.emit('myData',message,include_self=false); 
-	//io.emit('myData',message); 
-  });
-  
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Put all API endpoints under '/api'
+app.get('/api/passwords', (req, res) => {
+  const count = 5;
+
+  // Generate some passwords
+  const passwords = Array.from(Array(count).keys()).map(i =>
+    generatePassword(12, false)
+  )
+
+  // Return them as json
+  res.json(passwords);
+
+  console.log(`Sent ${count} passwords`);
 });
 
-io.on('connection', socket => {
-
-  socket.on('myData', boardId => {
-	console.log('aaa ', boardId);
-    socket.join(`board${boardId}`);
-  });
-
-  socket.on('myData', message => {
-	console.log('44444444444444 ', message);
-    //const room = `board${card.board}`;
-    //socket.broadcast.to(room).emit('card/create', card);
-	 
-	
-	setTimeout(() => {
-	  socket.broadcast.emit('myData',message);;
-	}, 50);
-  });
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/build/index.html'));
 });
 
+const port = process.env.PORT || 5000;
+app.listen(port);
 
-
-
-/*
-socket.sockets.on('connection', function (socket) {
-    console.log('A client is connected!');
-  socket.on('myData', message => {
-	console.log('bbb ', message);
-    //const room = `board${card.board}`;
-    //socket.broadcast.emit('myData',message);
-	//socket.broadcast.to(room).emit('card/create', card);
-  });	
-});
-*/
+console.log(`Password generator listening on ${port}`);
