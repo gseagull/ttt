8ab876fcb1956e8b64d18236eefe179715100303
guy shahaf <guy@algomizer.com>
2018-05-15 09:27:13 +0300

    a

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const io = require('socket.io')();
-const express = require('express')();
+//const express = require('express')();
 const port = process.env.PORT || 3000;
 var socket=io.listen(port);
 console.log('listening on port ', port);
 
-
+/*
 const server = express()
   .listen(port, () => console.log(`Listening on ${ port }`));
  
@@ -13,6 +13,7 @@ const io2 = socketIO(server);
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
+*/
 io.on('connection', (client) => {
   client.on('subscribeToTimer', (interval) => {
     console.log('client is subscribing to timer with interval ', interval);
